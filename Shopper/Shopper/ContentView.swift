0b1300fcad4cf609dbef64b208b5c657521db2e2
Mohit Gupta <Mohit.Gupta14@ibm.com>
2026-05-27 13:12:43 +0530

    Initial Project Setup

---
+++ Shopper/Shopper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shopper
+//
+//  Created by Mohit  on 27/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shopper/Shopper/ShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopperApp.swift
+//  Shopper
+//
+//  Created by Mohit  on 27/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
