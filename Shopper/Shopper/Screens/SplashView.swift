8a278a767bdf2274ef748c03d926c43e9e8ba9cd
Mohit Gupta <Mohit.Gupta14@ibm.com>
2026-05-27 13:54:26 +0530

    Added SplashView To the code

---
+++ Shopper/Shopper/Screens/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Shopper
+//
+//  Created by Mohit  on 27/05/26.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
